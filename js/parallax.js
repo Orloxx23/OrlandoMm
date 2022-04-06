2d43c58519d6180dbd715358e5a6add25ec539d8
Orlando Mina <madorlix@gmail.com>
2022-04-06 15:53:56 -0500

    Parallax mobile

--- js/parallax.js
+++ js/parallax.js
@@ -1,10 +1,10 @@
 var layer = document.getElementById('layer');
 var mainLayer = document.getElementById('main-layer');
 document.addEventListener('mousemove', function (e) {
-    var x = (e.pageX * -1 / 30);
-    var y = (e.pageY * -1 / 30);
-    var mx = (e.pageX * -1 / 100);
-    var my = (e.pageY * -1 / 100);
+    var x = (e.pageX * -1 / 25);
+    var y = (e.pageY * -1 / 25);
+    var mx = (e.pageX  / 50);
+    var my = (e.pageY / 50);
 
     this.querySelectorAll('#layer').forEach(layer => {
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
@@ -15,10 +15,10 @@ document.addEventListener('mousemove', function (e) {
 
 window.addEventListener('deviceorientation',function(e){
     console.log("ok");
-    const x = (e.gamma/5);
+    const x = (e.gamma/4);
     const y = (e.beta/4);
     var mx = (e.gamma/3);
-    var my = (e.beta/2);
+    var my = (e.beta/3);
     
     document.querySelectorAll('#layer').forEach(layer => {
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
