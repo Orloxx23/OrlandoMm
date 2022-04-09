e1c98897322c1637f1b13e0e5591c98cbb178102
Orlando Mina <madorlix@gmail.com>
2022-04-08 19:31:03 -0500

    Block scroll on mobile menu

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,12 @@
 const tabs = document.querySelectorAll('.tab');
 
-tabs.forEach(clickedTab =>{
-	clickedTab.addEventListener('click', () =>{
-		tabs.forEach(tab => {
-			tab.classList.remove('active');
-		});
-		clickedTab.classList.add('active');
-	});
+tabs.forEach(clickedTab => {
+    clickedTab.addEventListener('click', () => {
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        });
+        clickedTab.classList.add('active');
+    });
 });
 
 const observer = new IntersectionObserver((entries) => {
@@ -14,19 +14,19 @@ const observer = new IntersectionObserver((entries) => {
         const id = entry.target.getAttribute('id');
         const menuLink = document.querySelector(`.nav a[href="#${id}"]`);
 
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             document.querySelector('.nav a.active').classList.remove('active');
             menuLink.classList.add('active');
         }
     })
-}, {rootMargin: "-30% 0px -70% 0px"})
+}, { rootMargin: "-30% 0px -70% 0px" })
 
 const menuLinks = document.querySelectorAll('.nav a[href^="#"]');
 
 menuLinks.forEach((menuLink) => {
     const hash = menuLink.getAttribute('href');
     const target = document.querySelector(hash);
-    if(target){
+    if (target) {
         observer.observe(target);
     }
 })
@@ -36,15 +36,55 @@ const menu = document.querySelector('.nav-mobile');
 const overlay = document.querySelector('.overlay');
 const closenav = document.querySelector('.close-nav');
 
-function showMenu(){
-    menu.classList.add('nav-open');
-    overlay.classList.remove('hidden');
+function preventDefault(e) {
+    e.preventDefault();
 }
 
-function hideMenu(){
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false)
+}
+
+function showMenu() {
+    menu.classList.add('nav-open');
+    overlay.classList.remove('hidden');
+    disableScroll();
+}
+
+function hideMenu() {
     menu.classList.remove('nav-open');
     overlay.classList.add('hidden');
+    enableScroll();
 }
+
+
 
 const mobileTabs = document.querySelectorAll('.mobile-tab');
 
